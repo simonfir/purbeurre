77454c0def536e6eda3da75c0ba65ae6c1c43c8c
simon <simon.mesnier@orange.fr>
2020-04-03 17:37:48 +0200

    add main function, find_substitute and browse_saved_substitutes

--- main.py
+++ main.py
@@ -17,24 +17,53 @@ def numbered_choice(items):
     return int(answer) - 1
 
 
-def search_category():
-    print('Entrez votre recherche')
-    search = input('>>> ')
-    categories = db.search_categories(search)
-    if categories:
-        names = [cat.name for cat in categories]
-        return categories[numbered_choice(names)]
-    else:
-        print('Aucun résultats')
+def search_product():
+    """ Search products using user's key words. Return Product object
+    or None"""
+    while True:
+        print('Chercher un produit, entrez vos mots clés.')
+        search = input('>>> ')
+        products = db.search_products(search)
+        if products:
+            descriptions = [prod.short_description() for prod in products]
+            return products[numbered_choice(descriptions)]
+        else:
+            print('Aucun résultats')
 
 
-def select_product():
-    category = search_category()
-    if category:
-        products = category.products()
-        descriptions = [prod.short_description() for prod in products]
-        return products[numbered_choice(descriptions)]
+def find_substitute():
+    """ Help user find a substitute for a product"""
+    original = search_product()
+    print(original.description())
+    for substitute in original.substitutes():
+        print('Substitut possible:')
+        print(substitute.description())
+        choice = numbered_choice(('Enregistrer ce substitut',
+                                  'Voir un autre substitut',
+                                  'Retour'))
+        if choice == 0:
+            db.save_substitute(original, substitute)
+            print('Substitut sauvegardé')
+            break
+        elif choice == 2:
+            break
+
+
+def browse_saved_substitutes():
+    saved_substitutes = db.get_saved_substitutes()
+    names = [sub.short_description() for original, sub in saved_substitutes]
+    original, substitute = saved_substitutes[numbered_choice(names)]
+    print('Substitut pour ' + original.short_description())
+    print(substitute.description())
+
+
+def main():
+    while True:
+        choice = numbered_choice(('Quel aliment souhatez-vous remplacer?',
+                                  'Retrouver mes aliments substitués',
+                                  'Quitter'))
+        (find_substitute, browse_saved_substitutes, quit)[choice]()
 
 
 if __name__ == '__main__':
-    print(select_product().short_description())
+    main()
